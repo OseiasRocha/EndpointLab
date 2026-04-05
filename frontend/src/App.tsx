0aec5a72c501f3b883f72dd700d671170db51906
Oséias K. Rocha <oseiaskr95@gmail.com>
2026-04-05 14:28:21 +0400

    WIP
    Signed-off-by: Oséias K. Rocha <oseiaskr95@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,120 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React, { useState } from 'react'
 import './App.css'
+import { Button, Divider, InputAdornment, List, ListItem, Stack, TextField } from '@mui/material'
+import { Add, FilterList, Search } from '@mui/icons-material'
+
+function addEndpoint() {
+  console.log("Add endpoint called")
+  // send the endpoint object to backend
+}
+
+function search(msg: string) {
+  console.log(msg)
+  // send string to backend to search in DB
+}
+
+function endpointsList() {
+  return [
+  <ListItem>1</ListItem>,
+  <ListItem>6</ListItem>,
+  <ListItem>6</ListItem>,
+  <ListItem>7</ListItem>
+]
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const [endpoints] = useState()
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Stack>
+      <Stack direction="row">
+        <TextField color='secondary' variant='filled' label="Search" slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position='start'>
+                <Search color='secondary' />
+              </InputAdornment>
+            )
+          }
+        }} onChange={(event) => { search(event.target.value) }}>
+        </TextField>
+        <Button onClick={() => addEndpoint()} variant="contained" color='secondary' startIcon={<Add />}>Add</Button>
+      </Stack>
+      <Divider/>
+      <List sx={{maxHeight:500, overflow: 'auto'}}>
+      {
+        endpointsList()
+      }
+      </List>
+    </Stack>
   )
 }
 
