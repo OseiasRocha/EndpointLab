4d5989450f153c5601fdb9c5f0427b80bc39c5a1
Oséias K. Rocha <oseiaskr95@gmail.com>
2026-07-15 11:28:19 +0400

    Cleaning up the backend to start fresh
    Signed-off-by: Oséias K. Rocha <oseiaskr95@gmail.com>

--- backend/src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-
-export const endpoints = sqliteTable("endpoints", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name").notNull(),
-  protocol: text("protocol").notNull(), // tcp | udp
-  host: text("host").notNull(),
-  port: integer("port").notNull(),
-});
--- backend/src/repos/0000_large_talisman.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `endpoints` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`protocol` text NOT NULL,
-	`host` text NOT NULL,
-	`port` integer NOT NULL
-);
